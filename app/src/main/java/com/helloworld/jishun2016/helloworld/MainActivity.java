40ab329d44bfeda45694952712d61c0105f217e3
Shiling2017 <570601685@qq.com>
2017-06-28 15:48:22 +0800

    Initial commit

---
+++ app/src/main/java/com/helloworld/jishun2016/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package com.helloworld.jishun2016.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.gson.Gson;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       Gson gson=new Gson();
+    }
+}
